05b3fe6fc44c6aa054ef240bb23d5a39a84c27cf
Christopher Elliott <35berge35@gmail.com>
2018-03-27 20:07:01 -0400

    Added Chips call

---
+++ MaplicationAPI/Repositories/ChipRepository.cs
@@ -0,0 +1,25 @@
+﻿using MaplicationAPI.EntityFramework;
+using MaplicationAPI.Repositories.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaplicationAPI.Repositories
+{
+    public class ChipsRepository : IChipsRepository
+    {
+        private readonly EntityFramework.MaplicationContext _context;
+
+        public ChipsRepository(MaplicationContext context)
+        {
+            _context = context;
+        }
+
+        public List<Tags> BrowseChips()
+        {
+            return _context.Tags.AsNoTracking().ToList();
+        }
+    }
+}
